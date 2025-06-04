d5dbc03ef594170987a20003d6663263fa1f9725
nevcea <aduoss01@gmail.com>
2025-06-04 09:11:15 +0900

    chore(publisher-book-recommend): add book lists

--- app/publisher-book-recommend/page.js
+++ app/publisher-book-recommend/page.js
@@ -2,7 +2,13 @@
 
 import BookRecommend from './BookRecommend';
 
-const booksToSearch = [];
+const booksToSearch = [
+  "나의 투쟁 아돌프 히틀러"
+  "만화가가 말하는 만화가 나예리"
+  "나니아 연대기 C. S. 루이스"
+  "미래의 물리학 미치오 카쿠"
+  "삼국지 설민석"
+];
 
 export default function PublisherBookRecommend() {
   return (
